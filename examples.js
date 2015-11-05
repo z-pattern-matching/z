376a03520a49c131e5b6ebf17fe84c5a460c9f7b
Wagner Leonardi <wagner.leonardi@sprinklr.com>
2015-11-05 19:17:35 -0200

    new exercise add

--- examples.js
+++ examples.js
@@ -1,2 +1,11 @@
-require('z')
+require('./z')
+
+var maximum = function(list){
+  return list.$match(
+    function(x)    { return x },
+    function(x, xs){ return (x > maximum(xs)) ? x : maximum(xs)}
+  )
+}
+
+console.log(maximum([23, 89, 56, 70]))
 
