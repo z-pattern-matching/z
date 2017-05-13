b7b52cbaff27ef34e50ba98ba14f3392c9fceeec
Paulo Sousa <pauloj10@gmail.com>
2017-05-13 19:38:36 -0300

    Deal on  with options

--- src/match.js
+++ src/match.js
@@ -1,40 +1,41 @@
 const deepEqual = require('deep-equal')
+const option = require('./option')
 
 module.exports = (match, subjectToMatch) => {
   const hasMatchValue = match.args[0].length >= 2
   if(!hasMatchValue){
-    return subjectToMatch
+    return option.some(subjectToMatch)
   }
 
   const matchValue = match.args[0][1]
 
   //if is a type check, check type
-  if(matchValue === Boolean && typeof subjectToMatch === 'boolean') return subjectToMatch
+  if(matchValue === Boolean && typeof subjectToMatch === 'boolean') return option.some(subjectToMatch)
   //TODO check these!
-  //if(matchValue === Null && typeof subjectToMatch === 'string') return subjectToMatch
-  //if(matchValue === Undefined && typeof subjectToMatch === 'string') return subjectToMatch
-  if(matchValue === Number && typeof subjectToMatch === 'number') return subjectToMatch
-  if(matchValue === String && typeof subjectToMatch === 'string') return subjectToMatch
+  //if(matchValue === Null && typeof subjectToMatch === 'string') return option.some(subjectToMatch)
+  //if(matchValue === Undefined && typeof subjectToMatch === 'string') return option.some(subjectToMatch)
+  if(matchValue === Number && typeof subjectToMatch === 'number') return option.some(subjectToMatch)
+  if(matchValue === String && typeof subjectToMatch === 'string') return option.some(subjectToMatch)
   //TODO check it!
-  //if(matchValue === Symbol && typeof subjectToMatch === 'string') return subjectToMatch
-  if(matchValue === Object && typeof subjectToMatch === 'object') return subjectToMatch
+  //if(matchValue === Symbol && typeof subjectToMatch === 'string') return option.some(subjectToMatch)
+  if(matchValue === Object && typeof subjectToMatch === 'object') return option.some(subjectToMatch)
 
   //if is instance check, check instance
   if(typeof matchValue === "function" && subjectToMatch instanceof matchValue){
-    return subjectToMatch
+    return option.some(subjectToMatch)
   }
 
   //if is object or array check, check deep equality
   if(typeof subjectToMatch === 'object'){
     if(deepEqual(subjectToMatch, matchValue)){
-      return subjectToMatch
+      return option.some(subjectToMatch)
     }
   }
 
   //if is value check, check value
   if(subjectToMatch === matchValue){
-    return subjectToMatch
+    return option.some(subjectToMatch)
   }
 
-  return undefined
+  return option.none
 }
